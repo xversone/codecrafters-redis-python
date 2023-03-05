fc66351008a5aba61ca1e1ea383df945627b769a
xversone <58343507+xversone@users.noreply.github.com>
2023-03-05 14:21:40 +0100

    respond to PING

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, address = server_socket.accept() # wait for client
+            
+    client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
