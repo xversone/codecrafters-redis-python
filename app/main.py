4247a3b00f6a3683b74e64e463cbe5769b1e4e47
xversone <58343507+xversone@users.noreply.github.com>
2023-03-06 22:13:59 +0100

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
 BUFSIZE = 4096
+
+def response(client):
+    while input := client.recv(BUFSIZE):
+        client.send(b"+PONG\r\n")
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,11 +16,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, address = server_socket.accept() # wait for client
     
-    while input := client.recv(BUFSIZE):
-        client.send(b"+PONG\r\n")
-
+    while True:
+        client, address = server_socket.accept() # wait for client
+        t = threading.Thread(target=response, args=(client,)) 
+        t.start()
 
 if __name__ == "__main__":
     main()
